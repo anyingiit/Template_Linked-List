388aef8619383ce3e186d4f09b376c68bd54ffb1
anyingiit <49945850+anyingiit@users.noreply.github.com>
2021-01-10 18:13:03 +0800

    版本4- 可用

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include "main.h"
+#include "LinkedList.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    auto *myList = new LinkedList<int>(); //这里使用了auto, auto类型会根据赋值的类型自动确定类型.
+    cout<<myList->toString()<<endl;
+    for (int i = 1; i <= 5; i++) {
+        myList->addFirst(i);
+        cout<<myList->toString()<<endl;
+    }
+    myList->add(2, 999);
+    cout<<myList->toString()<<endl;
+//    cout<<myList->toString()<<endl;
+//    int a = 10;
+//    string *test = new string;
+//    *test = "abc";
+//    test->append("123");
+//    test->append(to_string(a) + "test");
+//    cout<<*test<<endl;
     return 0;
 }
--- main.h
+++ main.h
@@ -1,55 +1,56 @@
-#include <iostream>
-using namespace std;
+/*
+ * 学习版本
+ * 无法使用
+ * 可以看看注释, 但是程序有问题
+ */
 
-template<typename T>
-class MyNode{
-public:
-    T e;
-    MyNode<T> *next;
-    MyNode(T e,MyNode *next) : e(e),next(next){} //使用初始化列表构造
-    MyNode(T e) : e(e), next(nullptr){}
-    MyNode() : e(),next(nullptr){}
-//    Node(T e,Node *next);
-};
-
-template<typename T>
-class LinkedList{
-public:
-    LinkedList() {
-        head = nullptr;
-        size = 0;
-    }
-    int getSize() {
-        return size;
-    }
-    int isEmpty() {
-        return size == 0;
-    }
-    void addFirst(T e) {
-        head = new MyNode<T>(e,head); //将一个装有e元素并且指向到head的MyNode赋值给head, 使得head数据为新加入数据, 并且next指向到以前的head
-        size++; //大小增加1
-    }
-    void add(int index, T e) {
-        if (!(index > 0 && index <= size)){
-            //TODO:异常
-            cout<<"add时发现异常, 错误的index: "<<index<<endl;
-        }
-        if (index == 0){
-            addFirst(e);
-        } else{
-            MyNode<T> *prev = head;
-            for (int i = 0; i < index - 1; i++) { //1. 注意这里的结果最终实际指向的是index的前一个元素  2. 注意这里的index和数组的index不一样, 这里的index就是纯粹的从0开始数, 数到第index个元素. 例如add(index = 3,e = P);, 那么就是 A B →P← C D E F
-                prev = prev->next;
-            }
-            //这里的prev实际上就是index前一个元素.
-            prev->next = new MyNode<T>(e,prev->next); //新的MyNode元素的next指向的实际上就是原先next指向的元素.
-            size++;
-        }
-    }
-    void addLast(T e) {
-        add(size, e);
-    }
-private:
-    MyNode<T> *head;
-    int size;
-};
+//#include <iostream>
+//using namespace std;
+//
+//template<typename T>
+//class MyNode{
+//public:
+//    T e;
+//    MyNode<T> *next;
+//    MyNode(T e,MyNode *next) : e(e),next(next){} //使用初始化列表构造
+//    MyNode(T e) : e(e), next(nullptr){}
+//    MyNode() : e(),next(nullptr){}
+////    Node(T e,Node *next);
+//};
+//
+//template<typename T>
+//class LinkedList{
+//public:
+//    LinkedList() {
+//        dummyHead = new MyNode<T>();
+//        size = 0;
+//    }
+//    int getSize() {
+//        return size;
+//    }
+//    int isEmpty() {
+//        return size == 0;
+//    }
+//    void addFirst(T e) {
+//        add(0,e);
+//    }
+//    void add(int index, T e) {
+//        if (!(index > 0 && index <= size)){
+//            //TODO:异常
+//            cout<<"add时发现异常, 错误的index: "<<index<<endl;
+//        }
+//        MyNode<T> *prev = dummyHead;
+//        for (int i = 0; i < index; i++) { //1. 注意这里的结果最终实际指向的是index的前一个元素  2. 注意这里的index和数组的index不一样, 这里的index就是纯粹的从0开始数, 数到第index个元素. 例如add(index = 3,e = P);, 那么就是 A B →P← C D E F
+//            prev = prev->next;
+//        }
+//        //这里的prev实际上就是index前一个元素.
+//        prev->next = new MyNode<T>(e,prev->next); //新的MyNode元素的next指向的实际上就是原先next指向的元素.
+//        size++;
+//    }
+//    void addLast(T e) {
+//        add(size, e);
+//    }
+//private:
+//    MyNode<T> *dummyHead; //有了虚拟节点后, 相当于链表的实际长度总是+1的.
+//    int size;
+//};
